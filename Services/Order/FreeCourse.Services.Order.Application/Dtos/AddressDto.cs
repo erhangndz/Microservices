7befbd308d75bbf361bd58ddcf776d1f720b6d3a
Erhan Gündüz <erhangndz@live.com>
2023-11-05 16:38:49 +0300

    updated

---
+++ Services/Order/FreeCourse.Services.Order.Application/Dtos/AddressDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Application.Dtos
+{
+    public class AddressDto
+    {
+        public string Province { get;  set; }
+        public string District { get;  set; }
+        public string Street { get;  set; }
+        public string ZipCode { get;  set; }
+        public string AddressLine { get;  set; }
+    }
+}
