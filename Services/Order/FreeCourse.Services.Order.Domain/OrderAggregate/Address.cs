ac02b5571f138daec500abd10db16ba17e18ef8b
Erhan Gündüz <erhangndz@live.com>
2023-11-05 16:04:16 +0300

    order api

---
+++ Services/Order/FreeCourse.Services.Order.Domain/OrderAggregate/Address.cs
@@ -0,0 +1,38 @@
+﻿using FreeCourse.Services.Order.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Domain.OrderAggregate
+{
+    public class Address:ValueObject
+    {
+        public string Province { get; private set; }
+        public string District { get; private set; }
+        public string Street { get; private set; }
+        public string ZipCode { get; private set; }
+        public string AddressLine { get; private set; }
+
+        public Address(string province, string district, string street, string zipCode, string addressLine)
+        {
+            Province = province;
+            District = district;
+            Street = street;
+            ZipCode = zipCode;
+            AddressLine = addressLine;
+        }
+
+       
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Province;
+            yield return District;
+            yield return Street;
+            yield return ZipCode;
+            yield return AddressLine;
+        }
+    }
+}
---
+++ Services/Order/FreeCourse.Services.Order.Infrastructure/OrderDbContext.cs
@@ -0,0 +1,38 @@
+﻿using FreeCourse.Services.Order.Domain.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Order.Infrastructure
+{
+    public class OrderDbContext:DbContext
+    {
+
+        public const string DEFAULT_SCHEMA = "ordering";
+
+        public OrderDbContext(DbContextOptions<OrderDbContext> options): base(options) 
+        {
+            
+        }
+
+        public DbSet<Domain.OrderAggregate.Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().ToTable("Orders", DEFAULT_SCHEMA);
+            modelBuilder.Entity<OrderItem>().ToTable("OrderItems", DEFAULT_SCHEMA);
+
+            modelBuilder.Entity<OrderItem>().Property(x => x.Price).HasColumnType("decimal(18,2)");
+
+            modelBuilder.Entity<Domain.OrderAggregate.Order>().OwnsOne(x => x.Address).WithOwner();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
